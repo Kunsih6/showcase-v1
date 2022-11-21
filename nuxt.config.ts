3a588495e09ece20d82ab602972554a36eef5687
Estuardo Aguirre <EstuardoA25@gmail.com>
2022-11-21 08:58:47 -0600

    Update axios version, add access token

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [["@storyblok/nuxt", { accessToken: "" }]],
+  modules: [["@storyblok/nuxt", { accessToken: "Zy6sX1jp0xeGXpDfrnBdbAtt" }]],
 });
